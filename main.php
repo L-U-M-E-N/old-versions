4316cdc5f77d6dfd1008e54685d96cbe22a067e7
Elanis <elanis@hotmail.com>
2017-12-29 18:21:09 +0100

    Prepared to model update

--- main.php
+++ main.php
@@ -1,24 +1,11 @@
 <?php
 include('./lib/init.php');
 
-$currentpage = explode("?",$_SERVER['REQUEST_URI'])[0];
-// Strtolower
-$currentname = strtolower($currentpage);
-// Strip .* et /
-$currentname = str_replace(".php", "", $currentname);
-$currentname = str_replace(".html", "", $currentname);
-$currentname = str_replace(".htm", "", $currentname);
-$currentname = str_replace("/", "", $currentname);
-// Index verif
-if($currentname=="") {
-    $currentname = "index";
-}
-
 // Cache preparation
 $_SESSION['lang'] = (isset($_SESSION['lang']))?$_SESSION['lang']:'en';
 $_GET['page'] = (isset($_GET['page']))?$_GET['page']:1;
 
-$cache = 'cache/'.$currentname.'-'.$_SESSION['lang'].'-'.$_GET['page'].'.html';
+$cache = 'cache/'.$currentpage.'-'.$_SESSION['lang'].'-'.$_GET['page'].'.html';
 $expire = time()-$config['cache_time']; // valable un jour
 
 // Commandes Get
@@ -33,19 +20,19 @@ if(isset($_GET['cache']) && $_GET['cache']==0) {
 $blacklist = $config['cache_blacklist'];
 
 // Fichier existant ET temps depuis dernier cache inferieur a l'expiration ET pas dans la blacklist
-if(file_exists($cache) && filemtime($cache) > $expire && $config['cache_activated'] && array_search($currentname,$blacklist)===false && $cached==1) { 
+if(file_exists($cache) && filemtime($cache) > $expire && $config['cache_activated'] && array_search($currentpage,$blacklist)===false && $cached==1) { 
     readfile($cache);
 }
-elseif(array_search($currentname,$blacklist)!==false) {
-    include_once('basefiles/'.$currentname.'.php'); // We don't need to cache it
+elseif(array_search($currentpage,$blacklist)!==false) {
+    include_once('basefiles/'.$currentpage.'.php'); // We don't need to cache it
 }
 else {
-    if(!file_exists('basefiles/'.$currentname.'.php')) {
+    if(!file_exists('basefiles/'.$currentpage.'.php')) {
         include_once('errors/404.html');
     } else {    
         ob_start(); // ouverture du tampon
  
-        include_once('basefiles/'.$currentname.'.php');
+        include_once('basefiles/'.$currentpage.'.php');
 
         $page = ob_get_contents(); // copie du contenu du tampon dans une chaîne
         ob_end_clean(); // effacement du contenu du tampon et arrêt de son fonctionnement
