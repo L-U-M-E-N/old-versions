5a36bd12829efa595b5a918534d57c67f8438dd5
Elanis <elanis@hotmail.com>
2018-03-01 17:42:54 +0100

    New login design

--- basefiles/login.php
+++ basefiles/login.php
@@ -17,9 +17,44 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
 }
 ?>
 
-<form method="post" action="login">
-	<input type="text" name="username" id="username" placeholder="Username">
-	<input type="password" name="password" id="password" placeholder="Password">
+<style>
+	/**
+	 * Login
+	 */
+	#login {
+		position: fixed;
 
-	<input type="submit" value="Connect">
-</form>
+		top: calc(50% - 75px);
+		left: calc(50% - 150px);
+
+		width: 300px;
+		height: 150px;
+	}
+	#login input {
+		display: inline-block;
+
+		/*margin: 0.5em calc((300px - 4px - 1em - 225px)/2); /* Total - border - padding */
+		margin: 0.5em calc(37.5px - 0.5em);
+
+		padding: 0.5em;
+		border: 0;
+
+		background-color: #191919;
+		color: #eee;
+	}
+	#login input[type="password"], #login input[type="text"] {
+		width: 225px;
+	}
+	#login input[type="submit"] {
+		width: calc(225px + 1em);
+	}
+</style>
+
+<section id="login">
+	<form method="post" action="login">
+		<input type="text" name="username" id="username" placeholder="Username">
+		<input type="password" name="password" id="password" placeholder="Password">
+
+		<input type="submit" value="Connect">
+	</form>
+</section>
