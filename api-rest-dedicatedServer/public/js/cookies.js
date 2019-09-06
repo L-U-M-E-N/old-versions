c912aa4f7c7bc8d1e9c6bb9e2e56a66a6c6f6f48
Elanis <elanis@hotmail.com>
2019-09-06 17:36:16 +0200

    Updated custom framework

--- api-rest-dedicatedServer/public/js/cookies.js
+++ api-rest-dedicatedServer/public/js/cookies.js
@@ -1,49 +1,49 @@
 /**
- * Set a cookie.
+ * Set a cookie
  *
- * @param      {string}  cname   Cookie name
- * @param      {string}  cvalue  Cookie value
- * @param      {number}  exdays  Expiration day count
+ * @param     {string}  cname   Cookie name
+ * @param     {string}  cvalue  Cookie value
+ * @param     {number}  exdays  Expirations days
  */
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+function setCookie(cname, cvalue, exdays=30) {
+	if(cvalue === undefined || cvalue === null) { return; }
+
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	const expires = 'expires='+ d.toUTCString();
+
+	document.cookie = cname + '=' + cvalue + '; ' + expires;
+	document.cookie = cname + '=' + cvalue + '; ' + expires;
 }
 
 /**
- * Get a cookie's value
+ * Gets a cookie value by its name
  *
- * @param      {string}  cname   Cookie name
- * @return     {string}  Cookie's Value
+ * @param     {string}  cname   Cookie name
+ * @return   {string}  Cookie value
  */
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
-    }
-    return "";
+	const name = cname + '=';
+	const ca = document.cookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) === ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) === 0) {
+			return c.substring(name.length,c.length);
+		}
+	}
+	return '';
 }
 
 /**
- * Check if a cookie exists
+ * Checks if a cookie exists
  *
- * @param      {<type>}   cname   Cookie name
- * @return     {boolean}  Check if exists or not
+ * @param     {<type>}   cname   Cookie name
+ * @return   {boolean}  True if the cookie exists, false otherwise
  */
 function existCookie(cname) {
-    var content = getCookie(cname);
-    if (content != "" || content!=null) {
-        return true;
-    } else {
-        return false;
-    }
+	const content = getCookie(cname);
+	return (content !== '');
 }
